3a1e7d449a5ac0fd8b04d73eaa8f4c5763fd636f
Charles Karlo F. Leguarda <110873671+chrlskrlo@users.noreply.github.com>
2023-02-15 10:11:30 +0800

    Just manipulating the Text field and button for the add user
    practicing using tailwind CSS and material UI for future purposes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./input.css";
 import { useDispatch, useSelector } from "react-redux";
-import { addUser } from "./features/Users";
-import { useState } from "react";
+import { useState, } from "react";
+import { addUser, deleteUser, updateUsername } from "./features/Users";
+
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 
 
@@ -11,42 +15,77 @@ function App() {
 
   const [name, setName] = useState('');
   const [username, setUserName] = useState('');
+  const [newUsername, setNewUserName] = useState('');
 
 
   return (
-    <div className="flex justify-center items-center flex-col mt-2">
-      <div className="space-x-2">
-        <input className="border-2 border-black rounded" type='text'
-          placeholder="Name..."
-          onChange={
-            (event) => { setName(event.target.value) }} />
+    <div className="flex justify-center items-center flex-col mt-4">
+      <div className="flex align-center">
+        <Box
+          sx={{
+            '& > :not(style)': { m: 1 },
+          }}
+        >
+          <TextField
+            helperText="Please enter your name"
+            id="demo-helper-text-misaligned"
+            label="Name"
+            size='small'
+            onChange={
+              (event) => { setName(event.target.value) }}
+          />
 
-        <input
-        className="border-2 border-black rounded"
-          type='text'
-          placeholder="Username..."
-          onChange={
-            (event) => { setUserName(event.target.value) }} />
-
-        <button
-        className="rounded-full"
-          onClick={
-            () => {
-              dispatch(addUser({
-                id: userList[userList.length - 1].id + 1,
-                name, username
-              }))
-            }}>
-          Add user</button>
+          <TextField
+            helperText="Please enter your username"
+            id="demo-helper-text-misaligned"
+            label="username"
+            size='small'
+            onChange={
+              (event) => { setUserName(event.target.value) }}
+          />
+          {/* Material Button UI*/}
+          <Button
+            className="h-10"
+            size="small"
+            color='success'
+            variant="outlined"
+            onClick={
+              () => {
+                dispatch(addUser({
+                  id: userList[userList.length - 1].id + 1,
+                  name, username,
+                }))
+              }}
+          >Add user</Button>
+        </Box>
       </div>
 
+      {/*Userlist section. Mapping each list in userlist*/}
       <div>
         {userList.map((user) => {
           return (
-            <div className="mt-2 w-60 h-22 p-6 border-2 border-black">
-              <h1>{user.name}</h1>
-              <h1>{user.username}</h1>
-
+            <div className="p-5 m-5 rounded-xl border-2 border-black text-center ">
+              <p className="text-xl">{user.name}</p>
+              <p className="text-lg">{user.username}</p>
+              
+              <TextField
+            id="demo-helper-text-misaligned"
+            label="New username"
+            size='small'
+            onChange={
+              (event) => { setNewUserName(event.target.value) }}
+          />
+          <Button 
+          variant="text"
+          onClick={
+            () => {
+              dispatch(updateUsername({ id: user.id, username: newUsername }))
+            }}>Update Username</Button>
+          <Button 
+          variant="text"
+           color="error"
+           onClick={
+            () => { dispatch(deleteUser({ id: user.id })) }}>Delete Username</Button>
             </div>
           )
         })}
